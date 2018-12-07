0952f07b2209e502254b2440b9ab61bb7369df5a
zhou <zhou@qq.com>
2018-12-07 19:54:34 +0800

    二期项目

---
+++ resources/views/Admin/Admins/admins.blade.php
@@ -0,0 +1,154 @@
+@extends('Admin.Public.public')
+@section('content')
+<html>
+<head></head>
+<body>
+<div class="content">
+            <!-- 右侧内容框架，更改从这里开始 -->
+            <form class="layui-form xbs" action="">
+                <div class="layui-form-pane" style="text-align: center;">
+                  <div class="layui-form-item" style="display: inline-block;">
+                    <label class="layui-form-label xbs768">日期范围</label>
+                    <div class="layui-input-inline xbs768">
+                      <input class="layui-input" placeholder="开始日" id="LAY_demorange_s">
+                    </div>
+                    <div class="layui-input-inline xbs768">
+                      <input class="layui-input" placeholder="截止日" id="LAY_demorange_e">
+                    </div>
+                    <div class="layui-input-inline">
+                      <input type="text" name="username" placeholder="请输入用户名" autocomplete="off" class="layui-input">
+                    </div>
+                    <div class="layui-input-inline" style="width:80px">
+                        <button class="layui-btn" lay-submit="" lay-filter="sreach"><i class="layui-icon"></i></button>
+                    </div>
+                  </div>
+                </div> 
+            </form>
+            <xblock><button class="layui-btn layui-btn-danger" onclick="delAll()"><i class="layui-icon"></i>批量删除</button><button class="layui-btn" onclick="kiss_add('添加事件','/admins/create')"><i class="layui-icon"></i>添加</button><span class="x-right" style="line-height:40px">共有数据：88 条</span></xblock>
+            <table class="layui-table">
+                <thead>
+                    <tr>
+                        <th>
+                            <input type="checkbox" name="" value="">
+                        </th>
+                        <th>
+                            ID
+                        </th>
+                        <th>
+                            事件
+                        </th>
+                        <th>
+                            积分
+                        </th>
+                        <th>
+                            操作
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>
+                            <input type="checkbox" value="1" name="">
+                        </td>
+                        <td>
+                            1
+                        </td>
+                        <td>
+                            回复
+                        </td>
+                        <td>
+                            5
+                        </td>
+                        <td class="td-manage">
+                            <a title="编辑" href="javascript:;" onclick="kiss_edit('编辑','kiss-edit.html','4','','300')" style="text-decoration:none">
+                                <i class="layui-icon"></i>
+                            </a>
+                            <a title="删除" href="javascript:;" onclick="kiss_del(this,'1')" style="text-decoration:none">
+                                <i class="layui-icon"></i>
+                            </a>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+            <!-- 右侧内容框架，更改从这里结束 -->
+          </div>
+          </body>
+          <!-- 页面动态效果 -->
+    <!-- 页面动态效果 -->
+    <script>
+
+        layui.use(['laydate'], function(){
+          laydate = layui.laydate;//日期插件
+
+          //以上模块根据需要引入
+          //
+          
+
+          
+          var start = {
+            min: laydate.now()
+            ,max: '2099-06-16 23:59:59'
+            ,istoday: false
+            ,choose: function(datas){
+              end.min = datas; //开始日选好后，重置结束日的最小日期
+              end.start = datas //将结束日的初始值设定为开始日
+            }
+          };
+          
+          var end = {
+            min: laydate.now()
+            ,max: '2099-06-16 23:59:59'
+            ,istoday: false
+            ,choose: function(datas){
+              start.max = datas; //结束日选好后，重置开始日的最大日期
+            }
+          };
+          
+          document.getElementById('LAY_demorange_s').onclick = function(){
+            start.elem = this;
+            laydate(start);
+          }
+          document.getElementById('LAY_demorange_e').onclick = function(){
+            end.elem = this
+            laydate(end);
+          }
+          
+        });
+
+        //批量删除提交
+             function delAll () {
+                layer.confirm('确认要删除吗？',function(index){
+                    //捉到所有被选中的，发异步进行删除
+                    layer.msg('删除成功', {icon: 1});
+                });
+             }
+            // 等级-增加
+            function kiss_add (title,url,id,w,h) {
+                x_admin_show(title,url,w,h); 
+            }
+            // 等级-编辑
+            function kiss_edit (title,url,id,w,h) {
+                x_admin_show(title,url,w,h); 
+            }
+           
+            /*等级-删除*/
+            function kiss_del(obj,id){
+                layer.confirm('确认要删除吗？',function(index){
+                    //发异步删除数据
+                    $(obj).parents("tr").remove();
+                    layer.msg('已删除!',{icon:1,time:1000});
+                });
+            }
+        </script>
+        <script>
+        //百度统计可去掉
+        var _hmt = _hmt || [];
+        (function() {
+          var hm = document.createElement("script");
+          hm.src = "https://hm.baidu.com/hm.js?b393d153aeb26b46e9431fabaf0f6190";
+          var s = document.getElementsByTagName("script")[0]; 
+          s.parentNode.insertBefore(hm, s);
+        })();
+        </script>
+</html>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,11 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+// 后台控制器
+Route::group([],function(){
+	// 后台首页
+	Route::get('/admin','Admin\AdminController@index');
+	//后台管理员权限
+	Route::resource('/admins','Admin\AdminsController');
+});
